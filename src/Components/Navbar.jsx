7e1dc924dd2cf878f2dfaf483b2241620984ab70
shravit12 <138794026+shravit12@users.noreply.github.com>
2025-07-05 22:27:50 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
-import Logo from './logo.jsx';
+import Logo from './Logo.jsx';
 import '../navbarcs.css'
 
 const Navbar = ({ setActiveSection }) => {
