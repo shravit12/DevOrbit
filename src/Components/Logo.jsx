3b9961793134cd95c2d69f370a4fe2242a3aaac0
shravit12 <138794026+shravit12@users.noreply.github.com>
2025-07-05 22:40:35 +0530

    Update Logo.jsx

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -3,13 +3,23 @@
 const Logo = () =>{
   return[
     <>
-    <a className="navbar-brand fw-bold" href="./App.jsx"
-    style={{  fontFamily: "'Helvetica Neue', sans-serif", fontWeight: '500',
-      fontSize: '1.5rem', background: 'linear-gradient(to right,rgb(255, 255, 255),rgb(117, 117, 117))',
+  <a
+  className="navbar-brand fw-bold"
+  onClick={() => setActiveSection('home')}
+  style={{
+    fontFamily: "'Helvetica Neue', sans-serif",
+    fontWeight: 500,
+    fontSize: '1.25rem', // smaller and cleaner
+    background: 'linear-gradient(to right, rgb(255, 255, 255), rgb(117, 117, 117))',
     WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent' }}>
-        DevOrbit
-        </a>
+    WebkitTextFillColor: 'transparent',
+    cursor: 'pointer',
+    textDecoration: 'none'
+  }}
+>
+  DevOrbit
+</a>
+
     </>
   ]
 }
