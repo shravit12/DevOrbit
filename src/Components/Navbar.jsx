5328fab08f25b2fee7a1fdb9d2d21235b6178638
shravit12 <138794026+shravit12@users.noreply.github.com>
2025-07-06 16:05:13 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import Logo from './logo';
+import Logo from './Logo.jsx';
 import '../navbarcs.css';
 
 const Navbar = () => {
