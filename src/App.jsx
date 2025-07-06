5d055ded2c2ba4694f4bbc63b9dd2d69bc0eaf76
shravit12 <138794026+shravit12@users.noreply.github.com>
2025-07-06 15:46:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState , useEffect } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 import Navbar from './Components/Navbar.jsx';
-import Sidebar from './Components/Sidebar.jsx';
+import Sidebar from './components/Sidebar.jsx';
 import Home from './Components/Home.jsx';
 import About from './Components/About.jsx';
 import Projects from './Components/Projects.jsx';
@@ -14,34 +14,26 @@ import Help from "./pages/Help.jsx";
 
 import bgimage from './rm373batch9-035.jpg';
 
-
-
 function App() {
-     const [activeSection, setActiveSection] = useState(() => {
-    return sessionStorage.getItem('activeSection') || 'home';
-  });
-   
   useEffect(() => {
-  window.scrollTo(0, 0);
-}, [activeSection]);
-
-
-    const handleSectionChange = (section) => {
-    setActiveSection(section);
-    sessionStorage.setItem('activeSection', section);
-  };
+    window.scrollTo(0, 0);
+  }, []);
 
   return [
-    <div className="contain d-flex flex-column min-vh-100" 
-    style={{
-backgroundImage:`url(${bgimage})` , minHeight: "100vh",
-    backgroundSize: "cover",
-    backgroundAttachment: "fixed",
-    backgroundPosition: "center"}} >
-      <Navbar setActiveSection={handleSectionChange} />
-      <div className="d-flex flex-grow-1">
-        <Sidebar />
-         <div style={{ marginLeft: "200px", padding: "20px", width: "100%" }}>
+  
+      <div
+        className="contain d-flex flex-column min-vh-100"
+        style={{
+          backgroundImage: `url(${bgimage})`,
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center",
+        }}
+      >
+        <Navbar />
+        <div className="d-flex flex-grow-1">
+          <Sidebar />
+          <div style={{ marginLeft: "200px", padding: "20px", width: "100%" }}>
           <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/about" element={<About />} />
@@ -52,10 +44,10 @@ backgroundImage:`url(${bgimage})` , minHeight: "100vh",
   <Route path="/help" element={<Help />} />
 </Routes>
           </div>
+        </div>
       </div>
-     
-    </div>
-  ]
+   
+      ]
 }
 
 export default App;
