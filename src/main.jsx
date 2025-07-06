61384d48b5b5f7cbde4ed2391458b951b806afda
shravit12 <138794026+shravit12@users.noreply.github.com>
2025-07-06 15:52:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <BrowserRouter>
     <App />
+   </BrowserRouter>
   </StrictMode>,
 )
