621a3786bae9ed34cd7f5c05b490b122d79c3aa1
shravit12 <138794026+shravit12@users.noreply.github.com>
2025-07-08 14:05:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 import { useEffect ,useState} from 'react';
 import './App.css';
 
