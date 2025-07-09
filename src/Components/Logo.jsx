ce8122fcf9413f8b46aec7442ec560b60c6a674f
shravit12 <138794026+shravit12@users.noreply.github.com>
2025-07-09 21:13:31 +0530

    Update Logo.jsx

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -8,7 +8,7 @@ const Logo = () => {
       style={{
         fontFamily: "'Helvetica Neue', sans-serif",
         fontWeight: 500,
-        fontSize: '3.25rem',
+        fontSize: '2.25rem',
         background: 'linear-gradient(to right, rgb(255, 255, 255), rgb(117, 117, 117))',
         WebkitBackgroundClip: 'text',
         WebkitTextFillColor: 'transparent',
