3244bbddb6569d4749832c1ffbd915b14c67b624
shravit12 <138794026+shravit12@users.noreply.github.com>
2025-07-14 11:21:31 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,26 +4,20 @@ import ecomimage from '../6505894.jpg'
 import newsimge from '../22841.jpg'
 const projects = [
   {
-    title: 'Portfolio Website',
+    title: 'Create A Game Website',
     description: 'A personal website built using React and Bootstrap to showcase my work and skills.',
     image: portimge,
     link: 'https://your-portfolio-link.com',
     code: 'https://github.com/yourusername/portfolio'
   },
   {
-    title: 'E-Commerce Store',
+    title: 'Saloon website',
     description: 'A full-stack MERN store with product listings, cart, and Stripe payment integration.',
     image: ecomimage,
     link: 'https://your-store-link.com',
     code: 'https://github.com/yourusername/ecommerce-store'
-  },
-  {
-    title: 'News App',
-    description: 'A React app that shows the latest headlines and summaries using a news API.',
-    image: newsimge,
-    link: 'https://your-news-app.com',
-    code: 'https://github.com/yourusername/news-app'
-  },
+  }
+
 ];
 
 
